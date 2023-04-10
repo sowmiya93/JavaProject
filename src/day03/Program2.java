beab27d874f4ac44a97cee7b82a99c1ffb3340bc
sowmiya <sowmiyabalasubramaniyan93@users.noreply.github.com>
2023-04-10 20:34:29 +0530

    void usage
    while void using in main method it can not return any values so that we use the return keyword to return the values to those methods

---
+++ src/day03/Program2.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class Program2 {
+	public static void main(String[] args) 
+	{
+		int x=addFunction(10,50);
+		System.out.println(x);
+		System.out.println(x*x);
+	}
+
+public static int addFunction(int a,int b) 
+{
+	return (a+b);
+}
+}

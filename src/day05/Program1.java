6a1a3c7f14627ea1e929474c89ca67ff5419cefe
sowmiya <sowmiyabalasubramaniyan93@users.noreply.github.com>
2023-04-12 20:37:28 +0530

    method overloading
    it can only work on arguments and memory allocation not an access specifier
    it can overload in main method too

---
+++ src/day05/Program1.java
@@ -0,0 +1,36 @@
+package day05;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+				add(1,2,3);
+				add(2,3);
+				add(1.1,2.2,3.3);
+				add(2.3,3.4);
+			}
+
+
+			public static void add(int a,int b)
+			{
+				System.out.println(a+b);
+			}
+			
+			public static void add(int a, int b,int c)
+			{
+				System.out.println(a+b+c);
+			}
+			
+			public static void add(double a,double b)
+			{
+				System.out.println(a+b);
+			}
+			
+			public static void add(double a, double b,double c)
+			{
+				System.out.println(a+b+c);
+			}
+		}
+	
+
+
---
+++ src/day05/Program2.java
@@ -0,0 +1,44 @@
+package day05;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		  add(2d,3l); //d,d
+		  add(3,2f);// f,f
+		  add(2f,2l); //f,f
+		  add(3d,3f);// d,d
+		 
+		
+	}
+	
+	public static void add(byte a,byte b)
+	{
+		System.out.println(a+b);
+	}
+	
+	public static void add(short a,short b)
+	{
+		System.out.println(a+b);
+	}
+	
+	public static void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public static void add(long a,long b)
+	{
+		System.out.println(a+b);
+	}	
+	
+	public static void add(float a,float b)
+	{
+		System.out.println(a+b);
+	}
+	
+	public static void add(double a,double b)
+	{
+		System.out.println(a+b);
+	}
+	}
+
+

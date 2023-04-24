d2520b7da5bb09b6a6e05c65a0adea2e609996ce
sowmiya <sowmiyabalasubramaniyan93@users.noreply.github.com>
2023-04-24 20:15:26 +0530

    comparing using three variables

--- src/day09/Program1.java
+++ src/day09/Program1.java
@@ -3,7 +3,23 @@ package day09;
 public class Program1 {
 
 	public static void main(String[] args) {
+		System.out.println("Start Program...!");
+		int a=10;
+		int b=100;
+		int c=50;
 
+		if((a>b)&&(a>c))
+		{
+			System.out.println("a is greater");
+		}
+		else if((b>a)&&(b>c))
+		{
+			System.out.println("b is greater");
+		}
+		else {
+			System.out.println("c is greater");
+		}
+		
 	}
 
 }

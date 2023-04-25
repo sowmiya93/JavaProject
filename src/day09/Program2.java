4e0362c49b8d000b74d6b291a949fa8615c2068e
sowmiya <sowmiyabalasubramaniyan93@users.noreply.github.com>
2023-04-25 20:38:34 +0530

    Create Program2.java

---
+++ src/day09/Program2.java
@@ -0,0 +1,13 @@
+package day09;
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+Scanner scn=new Scanner(System.in);
+ String yname=scn.next();
+System.out.println(yname);
+
+	}
+
+}
